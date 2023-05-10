9c4f162bac886366dde2e0f0c0dc0f2589fa1784
Nacef19 <117386346+Nacef19@users.noreply.github.com>
2023-05-10 14:56:55 +0100

    Fourth  commit

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000
-const db = require("./databases/index");
+const db = require("./database/index");
 
 
 app.use(cors());
