ad58d4be454b88c62903f5c40c3d10982b735dda
MahdiNasri <mahdinasri304@gmail.com>
2023-05-11 13:30:04 +0200

    CRUD backend completed

--- schema.sql
+++ schema.sql
@@ -5,34 +5,38 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema OnlineCourses
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema onlinecourses
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema OnlineCourses
+-- Schema onlinecourses
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `OnlineCourses` DEFAULT CHARACTER SET utf8 ;
-USE `OnlineCourses` ;
+CREATE SCHEMA IF NOT EXISTS `onlinecourses` DEFAULT CHARACTER SET utf8mb3 ;
+USE `onlinecourses` ;
 
 -- -----------------------------------------------------
--- Table `OnlineCourses`.`users`
+-- Table `onlinecourses`.`users`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `OnlineCourses`.`users` (
-  `user-id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `onlinecourses`.`users` (
+  `idUser` INT NOT NULL AUTO_INCREMENT,
   `userFullName` VARCHAR(255) NOT NULL,
   `userEmail` VARCHAR(255) NOT NULL,
   `userPassword` VARCHAR(255) NOT NULL,
   `userRole` ENUM('teacher', 'student') NOT NULL,
   `userSection` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`user-id`),
+  PRIMARY KEY (`idUser`),
   UNIQUE INDEX `userEmail_UNIQUE` (`userEmail` ASC) VISIBLE)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `OnlineCourses`.`courses`
+-- Table `onlinecourses`.`courses`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `OnlineCourses`.`courses` (
+CREATE TABLE IF NOT EXISTS `onlinecourses`.`courses` (
   `idcourses` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(255) NOT NULL,
   `section` VARCHAR(255) NOT NULL,
@@ -43,36 +47,34 @@ CREATE TABLE IF NOT EXISTS `OnlineCourses`.`courses` (
   INDEX `fk_courses_users_idx` (`users_user-id` ASC) VISIBLE,
   CONSTRAINT `fk_courses_users`
     FOREIGN KEY (`users_user-id`)
-    REFERENCES `OnlineCourses`.`users` (`user-id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `onlinecourses`.`users` (`idUser`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `OnlineCourses`.`comments`
+-- Table `onlinecourses`.`comments`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `OnlineCourses`.`comments` (
+CREATE TABLE IF NOT EXISTS `onlinecourses`.`comments` (
   `idcomments` INT NOT NULL AUTO_INCREMENT,
   `comment` VARCHAR(255) NOT NULL,
   `users_user-id` INT NOT NULL,
   `courses_idcourses` INT NOT NULL,
-  `courses_users_user-id` INT NOT NULL,
-  PRIMARY KEY (`idcomments`, `users_user-id`, `courses_idcourses`, `courses_users_user-id`),
-  INDEX `fk_comments_courses1_idx` (`courses_idcourses` ASC, `courses_users_user-id` ASC) VISIBLE,
+  PRIMARY KEY (`idcomments`, `users_user-id`, `courses_idcourses`),
+  INDEX `fk_comments_courses1_idx` (`courses_idcourses` ASC) VISIBLE,
+  INDEX `fk_comments_users1` (`users_user-id` ASC) VISIBLE,
+  CONSTRAINT `fk_comments_courses1`
+    FOREIGN KEY (`courses_idcourses`)
+    REFERENCES `onlinecourses`.`courses` (`idcourses`),
   CONSTRAINT `fk_comments_users1`
     FOREIGN KEY (`users_user-id`)
-    REFERENCES `OnlineCourses`.`users` (`user-id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_comments_courses1`
-    FOREIGN KEY (`courses_idcourses` , `courses_users_user-id`)
-    REFERENCES `OnlineCourses`.`courses` (`idcourses` , `users_user-id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `onlinecourses`.`users` (`idUser`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+ 
+ 
---
+++ server/routes/postsRoutes.js
@@ -0,0 +1,9 @@
+const { getAllPosts, AddPosts, getTeacherPosts, DeletePosts, UpdatePosts } = require('../Controller.js/posts');
+
+const router = require('express').Router();
+
+router.get('/posts', getAllPosts)
+router.get('/posts/:id', getTeacherPosts)
+router.post('/posts', AddPosts)
+router.delete('/posts', DeletePosts)
+router.put('/posts',UpdatePosts)
