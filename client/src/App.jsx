090bd23b2f4dacf6d4fb58afb65b0703063200cd
MahdiNasri <mahdinasri304@gmail.com>
2023-05-14 03:36:28 +0200

    backend done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,52 +1,40 @@
-<<<<<<< Updated upstream
 import React , {useState , useEffect} from 'react';
 import {Routes , Route , BrowserRouter} from 'react-router-dom';
 import axios from 'axios';
-import './App.css';
 import AllPosts from './components/AllPosts';
 import Comment from './components/Comment';
 import CreatePosts from './components/CreatePosts';
 import UpdatePosts from './components/UpdatePosts';
-=======
-import Homepage from "./components/HomePage/Homepage";
-
-
->>>>>>> Stashed changes
 
 
 const App =() =>{
   const [data,setData] = useState ([]);
-  const [user ,setUser] = useState ([]);
-  useEffect (()=>{getPosts()},[]);
-  useEffect (()=>{getUsers()},[])
+  const[update,setUpdate] = useState([false])
+  useEffect (()=>{getPosts()},[update]);
+
   const getPosts = () => {
-    axios.get('http://localhost:3000/posts').then(response => {setData (response.data)})
+    axios.get('http://localhost:3000/api/posts').then(response => {
+      console.log("data",response.data)
+      setData(response.data)
+      console.log("2",data)
+    })
     .catch (error => {
       console.error(error)
     })
     }
-    const getUsers = () => {
-      axios.get('http://localhost:3000/users').then(response => {setData (response.data)})
-      .catch (error => {
-        console.error(error)
-      })
-      }
+    
   return (
-<<<<<<< Updated upstream
     <BrowserRouter><div className='app'>
       <Routes>
+      <Route path='/' element =  {<CreatePosts update={update} setUpdate={setUpdate} />}></Route>
         <Route path='/AllPosts' element =  {<AllPosts/>}></Route>
         <Route path='/Comment' element = {<Comment/>} ></Route>
-        <Route path='/CreatePosts' element = {<CreatePosts/>} ></Route>
+        {/* <Route path='/CreatePosts' element = {<CreatePosts/>} ></Route> */}
         <Route path='/UpdatePosts' element = {<UpdatePosts/>} ></Route>
       </Routes>
       </div></BrowserRouter>
   )
 
-=======
- <Homepage/>
-  );
->>>>>>> Stashed changes
 }
 
 export default App;
